cf6c0c3b931316f060db2dffbcf5e540d301f0fb
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-09 22:36:15 -0400

    Check two: at least two odd numbers is true

--- script.js
+++ script.js
@@ -6,4 +6,8 @@ const n4 = 5;
 
 // Check one: add up to 50?
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-console.log(isSum50)
+console.log(isSum50) // true
+
+// Check two: at least two odd numbers?
+const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
+console.log(isTwoOdd) // true
