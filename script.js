f2aadc141aeb294cb21306b7d0ca17899f2bdab5
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-09 22:30:46 -0400

    Added script.js to .html file and verified that js was live

---
+++ script.js
@@ -0,0 +1,9 @@
+// The initial numbers that must be verified.
+const n1 = 10;
+const n2 = 15;
+const n3 = 20;
+const n4 = 5;
+
+// Check one: add up to 50?
+const isSum50 = (n1 + n2 + n3 + n4) == 50;
+console.log(isSum50)
