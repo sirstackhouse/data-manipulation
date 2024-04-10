6f41f0546e5b38af0cdc00a6e34149d4887f8744
Idris Stackhouse <idris.stackhouse@gmail.com>
2024-04-09 22:44:56 -0400

    Check three: no number larger than 25, issue: Statement returns false even though all numbers are lower than 25

--- script.js
+++ script.js
@@ -11,3 +11,7 @@ console.log(isSum50) // true
 // Check two: at least two odd numbers?
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
 console.log(isTwoOdd) // true
+
+// Check three: no number larger than 25
+const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
+console.log(isOver25)
